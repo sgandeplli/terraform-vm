e6058a7865bbfa26fc9736d7f2458bb7f99bf672
sgandeplli <sgandepalli@softility.com>
2025-02-14 14:05:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+    project = "ferrous-upgrade-446608-k0"
+    region = "us-central1"
+    zone="us-central1-c"
+}
+
+resource "google_compute_instance" "test1" {
+  name="harness"
+  machine_type="e2-micro"
+  boot_disk{
+     initialize_params {
+      image = "centos-cloud/centos-stream-9"
+      }
+
+  }
+  network_interface{
+     network="default"
+     access_config{
+
+     }
+  }
+}
