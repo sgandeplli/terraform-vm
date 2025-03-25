fd1e77bec5a9a267845896d7047daf3ffe470100
sgandeplli <sgandepalli@softility.com>
2025-03-25 15:31:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,28 @@
+terraform {
+  backend "gcs" {
+    bucket  = "sekhar1913"  # Replace with your GCS bucket name
+    prefix  = "terraform/state"         # Folder inside the bucket
+  }
+}
+
 provider "google" {
-    project = "sekhar-452813"
-    region = "us-central1"
-    zone="us-central1-b"
+  project = "sekhar-452813"
+  region  = "us-east1"
 }
 
 resource "google_compute_instance" "test1" {
-  name="sivasai"
-  machine_type="e2-micro"
-  boot_disk{
-     initialize_params {
+  name         = "naruto"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
       image = "centos-cloud/centos-stream-9"
-      }
-
+    }
   }
-  network_interface{
-     network="default"
-     access_config{
 
-     }
+  network_interface {
+    network = "default"
+    access_config {
+    }
   }
 }
